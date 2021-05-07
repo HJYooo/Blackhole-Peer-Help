e1d5c7344b68bb8572d6f0c76a4662f16437d63d
HJYooo <90978368+HJYooo@users.noreply.github.com>
2021-05-07 13:49:24 +0000

    Added bower

--- packages/default/stonks/stonks.go
+++ packages/default/stonks/stonks.go
@@ -40,4 +40,4 @@ func Main(args map[string]interface{}) map[string]interface{} {
 	// 	return resp
 	// }
 
-	start := time.Now().Add(time.Hour * 24 * 30 * 
+	start := time.Now().Add(time.Hour * 24 * 30 * -1)
