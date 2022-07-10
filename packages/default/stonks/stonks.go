a66632a607eced4954dd6a94f736ecffa4bfa9e1
HJYooo <90978368+HJYooo@users.noreply.github.com>
2022-07-10 20:49:40 +0000

    Update module structure using msync.

--- packages/default/stonks/stonks.go
+++ packages/default/stonks/stonks.go
@@ -112,4 +112,4 @@ func getQuote(symbol string) YQuote {
 			Name:            q.ShortName,
 			Currency:        q.CurrencyID,
 			Trend:           GetTrendIcon(GetTrend(MarketChangePct)),
-			MarketState:     string(q.Mar
+			MarketState:     string(q.MarketS
