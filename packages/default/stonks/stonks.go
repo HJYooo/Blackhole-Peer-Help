a196487fdef39cf1f187a7dda89cf492d6089b39
HJYooo <90978368+HJYooo@users.noreply.github.com>
2022-08-07 11:55:07 +0000

    update logging

--- packages/default/stonks/stonks.go
+++ packages/default/stonks/stonks.go
@@ -114,3 +114,4 @@ func getQuote(symbol string) YQuote {
 			Trend:           GetTrendIcon(GetTrend(MarketChangePct)),
 			MarketState:     string(q.MarketState),
 			MarketPrice:     MarketPrice,
+			Ma
