ec781d19feb17f6c15b9842120631a00cc7d450c
HJYooo <90978368+HJYooo@users.noreply.github.com>
2023-03-16 21:29:25 +0000

    MAGETWO-87551: Convert existing data install/upgrade scripts

--- packages/default/stonks/template.go
+++ packages/default/stonks/template.go
@@ -8,3 +8,13 @@ import (
 )
 
 var fmap template.FuncMap
+
+func init() {
+	fmap = template.FuncMap{}
+	fmap["comma"] = func(n int) string {
+		in := strconv.FormatInt(int64(n), 10)
+		numOfDigits := len(in)
+		if n < 0 {
+			numOfDigits-- // First character is the - sign (not a digit)
+		}
+		numOfCommas := (numOfDigits - 1) / 3
